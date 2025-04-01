5ec46f2654029b462aeb01e6ab91c5d61fe5c7f6
Nanna Ketola <nannailona@gmail.com>
2025-04-01 12:57:08 +0300

    Set up enviroment variables

--- src/database_connection.py
+++ src/database_connection.py
@@ -1,11 +1,8 @@
-import os
 import sqlite3
+from config import DATABASE_PATH
 
-dirname = os.path.dirname(__file__)
 
-connection = sqlite3.connect(os.path.join(dirname, "..", "data", "database.sqlite"))
-
-# onnection = sqlite3.connect("test.sqlite")
+connection = sqlite3.connect(DATABASE_PATH)
 connection.row_factory = sqlite3.Row
 
 print(connection)
