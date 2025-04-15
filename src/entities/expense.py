73a7bd8a3e13eb3a5cbc45757ec20cdf073c7049
Nanna Ketola <nannailona@gmail.com>
2025-04-15 14:29:47 +0300

    Dete expense entity

--- src/entities/expense.py
+++
@@ -1,6 +0,0 @@
-class Expense:
-    def __init__(self, date, description, amount, user):
-        self.date = date
-        self.description = description
-        self.amount = amount
-        self.user = user
