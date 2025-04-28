c9cca0e7f26aa6e05dfd05a9e700b5395307f2bc
Nanna Ketola <nannailona@gmail.com>
2025-04-28 12:12:19 +0300

    Remove duplicate code

--- src/services/expense_service.py
+++ src/services/expense_service.py
@@ -28,7 +28,7 @@ class ExpenseService:
         expense_df = self._expense_repository.expenses_by_user(self._user)
         return expense_df
 
-    def add_expense(self, date, description, amount):
+    def create_expense(self, date, description, amount):
         user = self.get_current_user()
         return self._expense_repository.add_expense(date, description, amount, user)
 
@@ -63,9 +63,6 @@ class ExpenseService:
         self._user = user
 
         return user
-
-    def create_expense(self, date, description, amount):
-        self._expense_repository.add_expense(date, description, amount, self._user)
 
 
 expense_service = ExpenseService(default_user_repository, default_expense_repository)
